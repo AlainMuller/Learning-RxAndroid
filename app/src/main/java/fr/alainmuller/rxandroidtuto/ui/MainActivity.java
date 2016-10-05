d8b20620681831f723b0915f87be2ae04e8daf9b
Alain Muller <alain.muller@niji.fr>
2016-10-05 09:16:59 +0200

    Add a little bit of magic and eye candy

--- app/src/main/java/fr/alainmuller/rxandroidtuto/ui/MainActivity.java
+++ app/src/main/java/fr/alainmuller/rxandroidtuto/ui/MainActivity.java
@@ -1,14 +1,19 @@
 package fr.alainmuller.rxandroidtuto.ui;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Locale;
 
 import fr.alainmuller.rxandroidtuto.R;
+import fr.alainmuller.rxandroidtuto.data.GitHubMember;
 import fr.alainmuller.rxandroidtuto.manager.ApiManager;
 import rx.Observable;
 import rx.Observer;
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements Observer<String> 
 
     private static final String[] GITHUB_MEMBERS = new String[]{"AlainMuller", "JakeWharton", "cyrilmottier", "romainguy", "romannurik", "dwursteisen"};
     private TextView mMembersView;
+    private ProgressBar mProgressBar;
     private Subscription mSubscription;
     private long mStartTime;
 
@@ -32,15 +38,16 @@ public class MainActivity extends AppCompatActivity implements Observer<String> 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMembersView = (TextView) findViewById(R.id.textview);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress);
 
         mStartTime = System.currentTimeMillis();
 
         /* emits one String (userName) at a time from the constant */
         mSubscription = Observable.from(GITHUB_MEMBERS)
                 /* retrieve each GitHubMember based on an API call with the emitted userName*/
-                .flatMap(member -> ApiManager.getGitHubMember(member))
+                .flatMap(ApiManager::getGitHubMember)
                 /* convert each GitHubMember as a String to be displayed */
-                .map(member -> member.toString())
+                .map(GitHubMember::toString)
                 /* extra call to log data without altering this chain */
                 .doOnEach(title -> Log.d(MainActivity.class.getSimpleName(), "member retrieved : " + title))
                 /* aggregate all userNames to one single String */
@@ -66,17 +73,36 @@ public class MainActivity extends AppCompatActivity implements Observer<String> 
 
     @Override
     public void onCompleted() {
+        hideProgressBar();
         Toast.makeText(this, String.format(Locale.getDefault(), "Loading complete : %d items in %d ms", GITHUB_MEMBERS.length, System.currentTimeMillis() - mStartTime), Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onError(Throwable e) {
-        Toast.makeText(this, "Error : " + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+        hideProgressBar();
+        Toast.makeText(this, e.getLocalizedMessage() != null && !"".equals(e.getLocalizedMessage()) ? "Error : " + e.getLocalizedMessage() : "Unknown error", Toast.LENGTH_LONG).show();
         Log.e(MainActivity.class.getSimpleName(), "Exception : " + e.getLocalizedMessage());
+        e.printStackTrace();
     }
 
     @Override
     public void onNext(String members) {
         mMembersView.setText(members);
+    }
+
+    // *********************************************************************************************
+    // PRIVATE METHODS
+    // *********************************************************************************************
+
+    private void hideProgressBar() {
+        mProgressBar.animate().setDuration(500)
+                .alpha(0.0f)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        mProgressBar.setVisibility(View.GONE);
+                    }
+                });
     }
 }
